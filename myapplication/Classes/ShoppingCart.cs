abd77c58b0355fd0d3bdbbcf58f8087b1caa3635
Miguel Galvez <m.galvez.dev@gmail.com>
2025-06-30 21:13:26 -0700

    Prompted User for customer name and catalog id

--- myapplication/Classes/ShoppingCart.cs
+++ myapplication/Classes/ShoppingCart.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
 namespace myapplication.Classes
 {
     internal class ShoppingCart
--- myapplication/Program.cs
+++ myapplication/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using myapplication.Classes;
+using myapplication.Classes.Customer;
+
+
+
+Console.WriteLine("**********SHOPPING CART APPLICATION**********");
+// Create a new customer
+Console.WriteLine("Enter customer name: ");
+string customerName = Console.ReadLine() ?? "Default Customer";
+
+// Get customer catalog ID
+Console.WriteLine("Enter customer catalog ID: ");
+string catalogId = Console.ReadLine() ?? "12345";
+
+// Create customer object
+new Customer(customerName, "12345").DisplayCustomerInfo();
+
+
